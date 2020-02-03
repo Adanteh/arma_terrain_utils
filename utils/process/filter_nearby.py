60e7f2dba85baf31365f53af18a7b6ccb47f51ed
Adanteh <adanteh@gmail.com>
2020-02-03 16:21:07 +0100

    Change filter_nearby to gooey

--- utils/process/filter_nearby.py
+++ utils/process/filter_nearby.py
@@ -5,19 +5,27 @@
 """
 
 import argparse
+import sys
 from pathlib import Path
 from typing import Tuple
 
 from pandas import DataFrame
 from scipy import spatial
+from gooey import Gooey, GooeyParser
 
-from ..tb import load_tb, write_tb
+FOLDER = Path(__file__).parents[2]
+if str(FOLDER) not in sys.path:
+    sys.path.insert(0, str(FOLDER))
+
+from utils.tb import load_tb, write_tb, TbRow  # noqa: E402
+
 
 FOLDER = Path(__file__).parent
 
 
 class NearbyFiltering:
     RADIUS = 4
+    DESCRIPTION = "Allows you to automatically delete objects in a file, when they are closer than X distance to another file"
 
     def __init__(self, radius: float, source: DataFrame, target: DataFrame):
         self.r = radius
@@ -32,6 +40,7 @@ class NearbyFiltering:
     def filter_new(self):
         df = self.target
         remove_idx = []
+        row: TbRow
         for idx, row in df.iterrows():
             nearby = self.filter_nearby_points([row.x, row.y], self.RADIUS)
             if nearby:
@@ -46,27 +55,34 @@ class NearbyFiltering:
         nearby = self.tree.query_ball_point(point, radius)
         return nearby
 
-    def do_stuff(self, point):
-        print(point)
+
+    @classmethod
+    def parser(cls):
+        parser = GooeyParser(description=cls.DESCRIPTION)
+        parser.add_argument(
+            "source", help="The TB file you want to compare distances to", type=Path, widget="FileChooser"
+        )
+        parser.add_argument("target", help="The file you want to filter", type=Path, widget="FileChooser")
+        parser.add_argument("-r", "--radius", help="Radius", type=float, default=4)
+        return parser
+
+    @classmethod
+    def run(cls, args):
+        source = load_tb(args.source)
+        target = load_tb(args.target)
+
+        obj = cls(args.radius, source=source, target=args.target)
+        out = obj.filter_new()
+
+        outpath = args.target.with_name(args.target.stem + "_OUT.txt")
+        write_tb(outpath, out)
 
 
-def main(args):
-    target_path = Path(args.target[0])
-    source = load_tb(Path(args.source[0]))
-    target = load_tb(target_path)
-
-    obj = NearbyFiltering(args.r, source=source, target=target)
-    out = obj.filter_new()
-
-    outpath = target_path.with_name(target_path.stem + "_OUT.txt")
-    write_tb(outpath, out)
+@Gooey
+def cli():
+    parser = NearbyFiltering.parser()
+    NearbyFiltering.run(parser.parse_args())
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("source", help="The TB file you want to compare distances to", nargs=1)
-    parser.add_argument("target", help="The file you want to filter", nargs=1)
-    parser.add_argument("-r", help="Radius", type=float, default=4)
-    args = parser.parse_args()
-
-    main(args)
+    cli()
