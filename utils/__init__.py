4d6d1d1082537cfeb3fbb158d650cbc11658212f
Adanteh <adanteh@gmail.com>
2020-02-08 14:51:59 +0100

    Disable stacktrace for normal UI usage

--- utils/__init__.py
+++ utils/__init__.py
@@ -3,8 +3,14 @@
 # from .library.create_objects import main as create
 
 import colorama
+import sys
 from ansimarkup import AnsiMarkup, parse
-
+ 
+if "--debug" in sys.argv:
+    sys.argv.remove("--debug")
+else: 
+    sys.tracebacklimit = 0
+    
 colorama.init()
 user_tags = {
     "error": parse("<K><r><b>"),
